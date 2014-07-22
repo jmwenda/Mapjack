3f865e3575519a9aecc74e2bf4e8bf4587268766
Jude Mwenda <judemwenda@gmail.com>
2014-07-22 11:33:00 -0400

    Skeleton application with offline rendering for Mapjack

--- src/com/mapjack/MainActivity.java
+++ src/com/mapjack/MainActivity.java
@@ -5,30 +5,89 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.io.File;
+
+import org.mapsforge.core.model.LatLong;
+import org.mapsforge.map.android.graphics.AndroidGraphicFactory;
+import org.mapsforge.map.android.util.AndroidUtil;
+import org.mapsforge.map.android.view.MapView;
+import org.mapsforge.map.layer.cache.TileCache;
+import org.mapsforge.map.layer.renderer.TileRendererLayer;
+import org.mapsforge.map.model.common.PreferencesFacade;
+import org.mapsforge.map.rendertheme.InternalRenderTheme;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Environment;
+
 public class MainActivity extends ActionBarActivity {
+	// name of the map file in the external storage
+	        private static final String MAPFILE = "/locast/germany.map";
+	        
+	        private MapView mapView;        
+	        private TileCache tileCache;
+	        private TileRendererLayer tileRendererLayer;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+	        @Override
+	        protected void onCreate(Bundle savedInstanceState) {
+	                super.onCreate(savedInstanceState);
+	                
+	                AndroidGraphicFactory.createInstance(this.getApplication());
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
+	                this.mapView = new MapView(this);
+	                setContentView(this.mapView);
+
+	                this.mapView.setClickable(true);
+	                this.mapView.getMapScaleBar().setVisible(true);
+	                this.mapView.setBuiltInZoomControls(true);
+	                this.mapView.getMapZoomControls().setZoomLevelMin((byte) 10);
+	                this.mapView.getMapZoomControls().setZoomLevelMax((byte) 20);
+
+	                // create a tile cache of suitable size
+	                this.tileCache = AndroidUtil.createTileCache(this, "mapcache",
+	                                mapView.getModel().displayModel.getTileSize(), 1f, 
+	                                this.mapView.getModel().frameBufferModel.getOverdrawFactor());
+	        }
+
+	        @Override
+	        protected void onStart() {
+	                super.onStart();
+	                
+	                this.mapView.getModel().mapViewPosition.setCenter(new LatLong(52.517037, 13.38886));
+	                this.mapView.getModel().mapViewPosition.setZoomLevel((byte) 12);
+
+	                // tile renderer layer using internal render theme
+	                this.tileRendererLayer = new TileRendererLayer(tileCache,
+	                                this.mapView.getModel().mapViewPosition, false, AndroidGraphicFactory.INSTANCE);
+	                tileRendererLayer.setMapFile(getMapFile());
+	                tileRendererLayer.setXmlRenderTheme(InternalRenderTheme.OSMARENDER);
+	                
+	                // only once a layer is associated with a mapView the rendering starts
+	                this.mapView.getLayerManager().getLayers().add(tileRendererLayer);
+
+	        }
+
+	        @Override
+	        protected void onStop() {
+	                super.onStop();
+	                this.mapView.getLayerManager().getLayers().remove(this.tileRendererLayer);
+	                this.tileRendererLayer.onDestroy();
+	        }
+
+	        @Override
+	        protected void onDestroy() {
+	                super.onDestroy();
+	                this.tileCache.destroy();
+	        }
+	        
+	        private File getMapFile() {
+	                File file = new File(Environment.getExternalStorageDirectory(), MAPFILE);
+	                return file;
+	       }
 }
